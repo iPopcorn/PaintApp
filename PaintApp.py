8c00e4761d1916bcc2e9a38d46e78e259caeba61
iPopcorn <taypetrillo@gmail.com>
2016-10-11 10:52:40 -0500

    Changes: Basic drawing implemented Color selection popup implemented Shape selection popup implemented Canvas layout modified     Removed Label     Removed Color

--- PaintApp.py
+++ PaintApp.py
@@ -3,8 +3,44 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.graphics import Color, Ellipse, Line
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 
 Builder.load_file('PaintApp.kv')
+
+
+class ShapeSelectPopup(Popup):
+    pass
+
+
+class ColorSelectPopup(Popup):
+    pass
+
+
+class ToolbarButton(Button):
+    def colorSelectCallback(self):
+        print("colorSelectCallback()")
+        colorPopup = ColorSelectPopup()
+        colorPopup.open()
+    def shapeSelectCallback(self):
+        print("shapeSelectCallback()")
+        shapePopup = ShapeSelectPopup()
+        shapePopup.open()
+    def bucketToolCallback(self):
+        print("bucketSelectCallback()")
+    def undoToolCallback(self):
+        print("undoSelectCallback()")
+
+
+class CircleDraw(Widget):
+    def on_touch_down(self, touch):
+        color = (1,0,0)
+        with self.canvas:
+            Color(*color)
+            d = 30.
+            #Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d,d))
+            Line(circle=(touch.x, touch.y, d),width=2)
 
 
 class Toolbar(Widget):
@@ -25,9 +61,6 @@ class RootManager(ScreenManager):
         super(RootManager, self).__init__(**kwargs)
         rootScreen = RootScreen()
         self.add_widget(rootScreen)
-
-class Toolbar2(Widget):
-    pass
 
 
 class PaintApp(App):
